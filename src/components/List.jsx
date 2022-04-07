f911c678d8edddf1afea8e606d95a4eb95df1c95
ahmedalianz <ahmedalian400@gmail.com>
2022-04-07 15:31:55 +0200

    c

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,44 +1,53 @@
-import { Autoplay, Navigation } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-
 import ListItem from "./ListItem";
+import Slider from "react-slick";
 
 export default function List({ title, list }) {
+  let settings = {
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "ease-in-out",
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div className="list">
       <div className="list-container">
         <h1>{title}</h1>
         <div>
-          <Swiper
-            navigation
-            autoplay={{
-              delay: 2500,
-              disableOnInteraction: false,
-            }}
-            modules={[Navigation, Autoplay]}
-            centeredSlides
-            loop
-            breakpoints={{
-              640: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-              },
-              768: {
-                slidesPerView: 4,
-                spaceBetween: 40,
-              },
-              1024: {
-                slidesPerView: 5,
-                spaceBetween: 10,
-              },
-            }}
-          >
+          <Slider {...settings} className="swiper">
             {list.map((slide, i) => (
-              <SwiperSlide className="mySlide" key={i}>
+              <div className="mySlide" key={i}>
                 <ListItem slide={slide} />
-              </SwiperSlide>
+              </div>
             ))}
-          </Swiper>
+          </Slider>
         </div>
       </div>
     </div>
--- src/styles/CssImports.jsx
+++ src/styles/CssImports.jsx
@@ -4,11 +4,12 @@ import "./footer.scss";
 import "./navbar.scss";
 import "./featured.scss";
 import "./list.scss";
-import "swiper/css/bundle";
 import "./index.css";
 import "./watch.scss";
 import "./section1.scss";
 import "./frequent.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function CssImports() {
   return null;
